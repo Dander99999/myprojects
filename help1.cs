e8450588f70d52d38d601e6dd855163f1fca84ba
Dander99999 <cherednik.artm@bk.ru>
2024-06-07 21:56:58 +0300

    Add files via upload

---
+++ help1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Japanese_crossword
+{
+    public partial class help1 : Form
+    {
+        object obh;
+        object obh1;
+        public int idk;
+        public help1()
+        {
+            InitializeComponent();
+        }
+
+        private void help1_Load(object sender, EventArgs e)
+        {
+            obh = Properties.Resources.ResourceManager.GetObject("belka");
+            obh1 = Properties.Resources.ResourceManager.GetObject("lvl1");
+            DialogResult otvet = MessageBox.Show("Вы хотите не большую подсказку", "Подсказка", MessageBoxButtons.YesNo);
+            if(otvet == DialogResult.Yes)
+            {
+                pictureBox1.Image = (Bitmap)obh;
+                idk = 1;
+            }
+            else
+            {
+                pictureBox1.Image = (Bitmap)obh1;
+                idk = 0;
+            }
+        }
+    }
+}
---
+++ help2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Japanese_crossword
+{
+    public partial class help2 : Form
+    {
+        object obh;
+        object obh1;
+        public int idk;
+        public help2()
+        {
+            InitializeComponent();
+        }
+
+        private void help2_Load(object sender, EventArgs e)
+        {
+            obh = Properties.Resources.ResourceManager.GetObject("chess");
+            obh1 = Properties.Resources.ResourceManager.GetObject("lvl2");
+            DialogResult otvet = MessageBox.Show("Вы хотите не большую подсказку", "Подсказка", MessageBoxButtons.YesNo);
+            if (otvet == DialogResult.Yes)
+            {
+                pictureBox1.Image = (Bitmap)obh;
+                idk = 1;
+            }
+            else
+            {
+                pictureBox1.Image = (Bitmap)obh1;
+                idk = 0;
+            }
+        }
+    }
+}
